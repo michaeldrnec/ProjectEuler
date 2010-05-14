528205f06bee4e6223b6d13452cfa0d402e535ba
Michael Drnec <mdrnec@geolearning.com>
2010-05-14 15:06:22 -0500

    Wrote prime sieve algorithm TDD. Rocked it...

--- Helper.rb
+++ Helper.rb
@@ -9,3 +9,28 @@ class String
 		split("").inject(0) {|sum, n| sum + (n[0] - 64)}
 	end
 end
+
+class Primes
+	def GeneratePrimesToNumber(value)
+		sieve = Array.new
+		for i in (1..value)
+			sieve << i
+		end
+		start = 2
+		while (start * start <= value)
+			current = start * start
+			while (current <= value)
+				sieve[current - 1] = 0
+				current = current + start
+			end
+			start = start + 1
+		end
+		answers = Array.new
+		for x in (2..sieve.length)
+			if (sieve[x-1] != 0)
+				answers << sieve[x-1]
+			end
+		end
+		return answers
+	end
+end
--- Problem31.rb
+++ Problem31.rb
@@ -1,15 +1,20 @@
 # How many combinations of 1p, 2p, 5p, 10p, 20p, 50p, 1 pound, and 2 pound make 2 pounds in UK
 class Problem31
 	def TheAnswer()
+		max = 200
 		coins = Array.new(8)
-		coins[0] = 200
-		coins[1] = 100
-		coins[2] = 40
-		coins[3] = 20
-		coins[4] = 10
-		coins[5] = 4
-		coins[6] = 2
-		coins[7] = 1
+		coins[0] = 1
+		coins[1] = 2
+		coins[2] = 5
+		coins[3] = 10
+		coins[4] = 20
+		coins[5] = 50
+		coins[6] = 100
+		coins[7] = 200
+		for each c in coins
+			
+		end
+		return "unknown"
 	end
 end
 
